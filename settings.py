e438c934fd00cfb43df73ee9472aa843156c8e64
虞四明 <13966931523@163.com>
2019-01-12 15:25:33 +0800

    创建游戏设置类，统一管理游戏的各种设置

---
+++ settings.py
@@ -0,0 +1,8 @@
+class Settings():
+    """统一管理游戏的设置"""
+
+    def __init__(self):
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
+        self.caption = "外星人入侵"
