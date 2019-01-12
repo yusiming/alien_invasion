11a435fb1a1d30c877aec9b4fdd5161c867e1712
虞四明 <13966931523@163.com>
2019-01-12 19:41:06 +0800

    修改错误

--- bullet.py
+++ bullet.py
@@ -9,7 +9,7 @@ class Bullet(Sprite):
         super(Bullet, self).__init__()
         self.screen = screen
         # 创建一个表示子弹的矩形
-        self.rect = pygame.Rect(0, 0, ai_settings.self.bullet_width, ai_settings.self.bullet_height)
+        self.rect = pygame.Rect(0, 0, ai_settings.bullet_width, ai_settings.bullet_height)
         # 设置子弹的位置
         self.rect.centerx = ship.rect.centerx
         self.rect.top = ship.rect.top
