f1bd6b63deed942de9357e5c591ca08606f4e752
虞四明 <13966931523@163.com>
2019-01-12 19:15:56 +0800

    添加子弹设置

--- settings.py
+++ settings.py
@@ -7,3 +7,8 @@ class Settings():
         self.bg_color = (230, 230, 230)
         self.caption = "外星人入侵"
         self.ship_speed_factor = 1.5
+        # 子弹设置
+        self.bullet_speed_factor = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = 60, 60, 60
