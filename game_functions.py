965bdd827a3bf9818b1a059849097071040e1cf1
虞四明 <13966931523@163.com>
2019-01-12 19:42:06 +0800

    响应空格按键，更新子弹位置

--- game_functions.py
+++ game_functions.py
@@ -1,20 +1,21 @@
 import sys
 import pygame
+from bullet import Bullet
 
 
-def check_events(ship):
+def check_events(ai_settings, screen, ship, bullets):
     """响应鼠标和键盘事件"""
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            check_keydown_event(event, ship)
+            check_keydown_event(event, ai_settings, screen, ship, bullets)
         elif event.type == pygame.KEYUP:
             check_keyup_event(event, ship)
 
 
-def check_keydown_event(event, ship):
+def check_keydown_event(event, ai_settings, screen, ship, bullets):
     """检测KEYDOWN事件"""
     if event.key == pygame.K_RIGHT:
         # 将飞船的右移标志设置为True
@@ -26,6 +27,9 @@ def check_keydown_event(event, ship):
         ship.moving_down = True
     elif event.key == pygame.K_UP:
         ship.moving_up = True
+    elif event.key == pygame.K_SPACE:
+        # 创建一颗子弹，并将其加入到编组bullets中
+        bullets.add(Bullet(ai_settings, screen, ship))
 
 
 def check_keyup_event(event, ship):
@@ -42,10 +46,12 @@ def check_keyup_event(event, ship):
         ship.moving_up = False
 
 
-def update_screen(ai_settings, screen, ship):
+def update_screen(ai_settings, screen, ship, bullets):
     """更新图像，绘制屏幕"""
 
     screen.fill(ai_settings.bg_color)
     ship.blit_me()
+    for bullet in bullets.sprites():
+        bullet.draw_bullet()
     # 让最近绘制的屏幕可见
     pygame.display.flip()
