415b96ef91acf9d5ab73c83c808c7e99201a1db1
虞四明 <13966931523@163.com>
2019-01-12 15:21:22 +0800

    修改窗口背景颜色，添加注释

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,10 +3,14 @@ import pygame
 
 
 def run_game():
-    pygame.init()
+    """游戏的主函数，从这里开始运行游戏"""
 
+    pygame.init()
     screen = pygame.display.set_mode((1200, 800))
     pygame.display.set_caption("外星人入侵")
+    bg_color = (230, 230, 230)
+    # 设置窗口背景颜色
+    screen.fill(bg_color)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
